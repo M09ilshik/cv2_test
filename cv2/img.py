91555649f0c3be937ad07b026c9c44fec488c67d
M09ilshik <148627539+M09ilshik@users.noreply.github.com>
2024-02-10 16:47:58 +0500

    Add files via upload

--- cv2/img.py
+++ cv2/img.py
@@ -6,7 +6,7 @@ import cv2
 #
 # cv2.waitKey(5000)
 
-img1 = cv2.imread("cv2/img&vid/face.JPG")
+img1 = cv2.imread("img&vid/face.JPG")
 img1 = cv2.resize(img1, (img1.shape[1], img1.shape[0]))
 # img1 = cv2.GaussianBlur(img1, (15, 15), 0)
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
--- cv2/video.py
+++ cv2/video.py
@@ -1,10 +1,35 @@
 import cv2
+import numpy as np
 
+cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture("img&vid/video.mp4")
+# cap = cv2.resize(cap, (cap.shape[1] * 2, cap.shape[0] * 2))
+
 
 while True:
+
     success, img = cap.read()
     cv2.imshow("Video", img)
+    # img = cv2.GaussianBlur(img, (15, 15), 0)
+    r = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+    cv2.imshow("Video2", r)
 
-    cv2.waitKey(10)
+    g = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
+    cv2.imshow("Video3", g)
+
+    b = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    b = cv2.Canny(b, 100, 100)
+    cv2.imshow("Video4", b)
+    kernel = np.ones((3, 3), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+
+
+
+
+
+
+    cv2.waitKey(13)
+
+
